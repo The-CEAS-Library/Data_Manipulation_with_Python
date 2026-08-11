d4130b7923eeb44754429af3c24cc15c3e37dc4b
SamHelmrath <122654790+SamHelmrath@users.noreply.github.com>
2026-08-11 11:17:47 -0400

    Update survey_years.py

--- Exercise Solutions/survey_years.py
+++ Exercise Solutions/survey_years.py
@@ -1,32 +1,29 @@
-# import the pandas and os package and module
 import pandas as pd, os
 
-# Create a new folder called yearly_files with path 'data/yearly_files'
-os.mkdir('data/yearly_files')
+os.mkdir('yearly_files')
 
-# Function to generate file for each year
 def generate_year_files():
-    # Read in the "data/surveys.csv" as a DataFrame
-    surveys_df = pd.read_csv("data/surveys.csv")
-    
-    # Use the for loop to process each unique year
-    for year in pd.unique(surveys_df['year']):
-        # use the correct condition to filter out the years between 1990 and 2000
-        if year >= 1990 and year <= 2000:
-            # form a pandas query to get data for one particular year
-            survey_year_df = surveys_df[surveys_df['year'] == year]
-            
-            # create a file path name for the yaer with the format
-            # 'data/yearly_files/surveys_year.csv'
-            # Example ''data/yearly_files/surveys_1977.csv''
-            file_path_name = 'data/yearly_files/surveys_' + str(year) + '.csv'
-            
-            #Export the data in the survey_year_df DataFrame for the year to the file path craeted above
-            survey_year_df.to_csv(file_path_name)
 
-# Function to print out file names for each year
+  surveys_df = pd.read_csv('surveys.csv')
+
+  unique_years = surveys_df['year'].unique()
+
+  for year in unique_years:
+
+    if year >= 1990 and year <= 2000:
+
+      yearly_data = surveys_df[surveys_df.year == year]
+
+      file_name = 'yearly_files/survey_'+str(year)+'.csv'
+
+      yearly_data.to_csv(file_name)
+
+  return 'Done exporting csv files'
+
 def display_year_file_names():
-    # use the for loop to loop through the list created by os.listdir('data/yearly_files')
-    for file_name in os.listdir('data/yearly_files'):
-        # print each file name
-        print(file_name)
+
+  file_names = os.listdir('yearly_files')
+
+  for name in file_names:
+
+    print(name)
